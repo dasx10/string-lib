cb4aa25539df367d9fcfd91610f679f1d10e8e11
developer <=>
2021-03-05 14:17:31 +0200

    types

--- @types.ts
+++ @types.ts
@@ -1,21 +1,21 @@
 interface String {
     log():string;
 
-    toCamelCase(symbol:string):string;
+    toCamelCase(symbol?:string):string;
     toCapitalizeAll():string;
     toCapitalize():string;
 
-	repeat(num:number):string;
+	repeat(num?:number):string;
     reverse():string;
     limit():string;
 
-    count(str:string):number;
+    count(str?:string):number;
     isEmpty():boolean;
 
-    toRegEx(params:string):RegExp;
+    toRegEx(params?:string):RegExp;
     toJson():string|object;
 
-    toSplit(n:number):Array<string>;
+    toSplit(n?:number):Array<string>;
 
     toCharCodes():Int32Array;
 }
